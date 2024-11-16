da3fa090ede135c145bb976f308693ee98ac395d
sain2808 <hoangdat753@gmail.com>
2024-11-16 08:42:58 +0700

    -a

--- Source .cpp
+++ Source .cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-int main()
+
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a;
+	cout<< "Nhap a;
+	cin>> a;
 
 	system("pause");
 	return 0;
